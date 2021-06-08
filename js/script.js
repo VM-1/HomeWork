4bde7e74be7106510baacba37f6c9e9627e0ad2a
Valiko <57510048+VM-1@users.noreply.github.com>
2021-06-08 08:45:09 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// Filter method
+console.log("Filter method");
+console.log("\n");
+
+
+function filter(array, func, startIndex = 0) {
+  if (array.length > 1) {
+    let result = [];
+    for (let i = startIndex; i < array.length; i++) {
+      if (func(array[i])) {
+        result.push(array[i])
+      }
+    }
+    return result;
+  } else return null
+
+}
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+
+let result = filter(arr, (x) => {
+    return x % 2 === 0
+}, startIndex = 2)
+
+console.log(result);
+
+
+
